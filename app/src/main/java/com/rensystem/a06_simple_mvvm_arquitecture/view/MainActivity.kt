c1e16198b7ae5a6e8b6f77826bb0192551bd8ab5
RendevMq <rensystem73@gmail.com>
2025-03-07 19:57:10 -0500

    flow with comments

--- app/src/main/java/com/rensystem/a06_simple_mvvm_arquitecture/view/MainActivity.kt
+++ app/src/main/java/com/rensystem/a06_simple_mvvm_arquitecture/view/MainActivity.kt
@@ -3,15 +3,16 @@ package com.rensystem.a06_simple_mvvm_arquitecture.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.rensystem.a06_simple_mvvm_arquitecture.databinding.ActivityMainBinding
 import com.rensystem.a06_simple_mvvm_arquitecture.viewmodel.QuoteViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    // Delegado viewModels que crea y vincula el ViewModel a la actividad
+    // Se usa el delegado viewModels para obtener el ViewModel
     private val quoteViewModel: QuoteViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Oservamos los cambios en LiveData y actualizamos la UI
-        quoteViewModel.quoteModel.observe(this) { quote ->
-            // Si la cita no es nula, actualizamos la UI con los datos
-            quote?.let {
-                binding.tvQuote.text = it.quote
-                binding.tvAuthor.text = it.author
+        // Usamos lifecycleScope para manejar la coroutine de forma segura dentro del ciclo de vida
+        lifecycleScope.launch {
+            quoteViewModel.quoteModel.collect{ quote ->
+                // Si la cita (quote) no es nula, actualizamos la UI con los datos
+                quote?.let {
+                    binding.tvQuote.text = it.quote
+                    binding.tvAuthor.text = it.author
+                }
             }
         }
 
--- app/src/main/java/com/rensystem/a06_simple_mvvm_arquitecture/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/rensystem/a06_simple_mvvm_arquitecture/viewmodel/QuoteViewModel.kt
@@ -1,22 +1,24 @@
 package com.rensystem.a06_simple_mvvm_arquitecture.viewmodel
-
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.rensystem.a06_simple_mvvm_arquitecture.model.QuoteModel
 import com.rensystem.a06_simple_mvvm_arquitecture.model.QuoteProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class QuoteViewModel : ViewModel() {
 
-    //MutableLiveData qu contiene la cita actual
-    private val _quoteModel = MutableLiveData<QuoteModel>()
+    //Se crea un StateFlow que contiene el QuoteModel inicializado como null
+    private val _quoteModel = MutableStateFlow<QuoteModel?>(null)
 
-    //LiveData inmutable expuesto
-    val quoteModel: LiveData<QuoteModel> = _quoteModel
+    // Exposición del StateFlow como un StateFlow inmutable
+    // permitiendo que otros componentes solo lean el valor y no lo modifiquen directamente.
+    val quoteModel: StateFlow<QuoteModel?> = _quoteModel
 
-    fun randomQuote(){
+    // Función que genera una nueva cita aleatoria y actualiza el valor de _quoteModel.
+    fun randomQuote() {
         val currentQuote = QuoteProvider.random()
-        _quoteModel.postValue(currentQuote) // postValue se usa para cambiar el valor desde un hilo no principal
+        _quoteModel.value = currentQuote // Actualiza el valor de _quoteModel con la nueva cita.
     }
 
 }
